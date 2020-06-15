707ad7174b878619122bd698c423ff6ebd64df7e
nilayingle <nilayingle18@gmail.com>
2020-06-15 20:47:44 +0530

    Added Column

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,19 @@ class MyHome extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
       body: Center(
-        child: Text('Hello World!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Nilay Ingle'),
+            FlatButton(
+              child: Text('Click Here'),
+              onPressed: () => print('Button Pressed'),
+              color: Colors.red,
+              textColor: Colors.white,
+            ),
+            Text('Nilay Tech')
+          ],
+        ),
       ),
     );
   }
