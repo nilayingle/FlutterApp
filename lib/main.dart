48646a6f7c80d0de7cfb32c39bbc77ffd2cc1fb2
nilayingle <nilayingle18@gmail.com>
2020-06-15 20:32:37 +0530

    Initial Commit - Hello World

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FirstApp',
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
