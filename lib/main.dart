0d04f96dd0c321ea27c635aa21609d03b57b2504
nilayingle <nilayingle18@gmail.com>
2020-06-16 13:21:28 +0530

    Used Text, TextStyle, RichText, TextSpan Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -27,25 +27,65 @@ class _MyHomeState extends State<MyHome> {
         backgroundColor: Colors.green,
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FlatButton(
-              child: Text('Click Here'),
-              onPressed: () => Toast.show(
-                'Toast Demo', 
-                context,
-                duration: Toast.LENGTH_LONG, 
-                gravity: Toast.BOTTOM,
-                backgroundColor: Colors.blue,
-                textColor: Colors.white,
-              ),
-              color: Colors.red,
-              textColor: Colors.white,
+        child: RichText(
+          text: TextSpan(
+            text: 'Nilay',
+            style: TextStyle(
+              color: Colors.cyan,
+              fontSize: 15,
             ),
-          ],
+            children: <TextSpan>[
+              TextSpan(
+                text: 'Ingle : ',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30,
+                ),
+              ),
+              TextSpan(
+                text: 'Flutter',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 20,
+                ),
+              ),
+            ]
+          ),
         ),
       ),
+      // body: Center(
+      //   child: Text(
+      //     'Nilay Ingle',
+      //     maxLines: 3,
+      //     style: TextStyle(
+      //       backgroundColor: Colors.red,
+      //       color: Colors.white,
+      //       fontWeight: FontWeight.bold,
+      //       fontSize: 30,
+      //       fontStyle: FontStyle.italic,
+      //     ),
+      //   ),
+      // ),
+      // body: Center(
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       FlatButton(
+      //         child: Text('Click Here'),
+      //         onPressed: () => Toast.show(
+      //           'Toast Demo', 
+      //           context,
+      //           duration: Toast.LENGTH_LONG, 
+      //           gravity: Toast.BOTTOM,
+      //           backgroundColor: Colors.blue,
+      //           textColor: Colors.white,
+      //         ),
+      //         color: Colors.red,
+      //         textColor: Colors.white,
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
