d294f9231e115ff90e2966d2f81f5a35fe046d27
nilayingle <nilayingle18@gmail.com>
2020-06-16 13:28:46 +0530

    Seperated Home Widget

---
+++ lib/my_home_app.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: RichText(
+          text: TextSpan(
+            text: 'Nilay',
+            style: TextStyle(
+              color: Colors.cyan,
+              fontSize: 15,
+            ),
+            children: <TextSpan>[
+              TextSpan(
+                text: 'Ingle : ',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 30,
+                ),
+              ),
+              TextSpan(
+                text: 'Flutter',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 20,
+                ),
+              ),
+            ]
+          ),
+        ),
+      ),
+      // body: Center(
+      //   child: Text(
+      //     'Nilay Ingle',
+      //     maxLines: 3,
+      //     style: TextStyle(
+      //       backgroundColor: Colors.red,
+      //       color: Colors.white,
+      //       fontWeight: FontWeight.bold,
+      //       fontSize: 30,
+      //       fontStyle: FontStyle.italic,
+      //     ),
+      //   ),
+      // ),
+      // body: Center(
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       FlatButton(
+      //         child: Text('Click Here'),
+      //         onPressed: () => Toast.show(
+      //           'Toast Demo', 
+      //           context,
+      //           duration: Toast.LENGTH_LONG, 
+      //           gravity: Toast.BOTTOM,
+      //           backgroundColor: Colors.blue,
+      //           textColor: Colors.white,
+      //         ),
+      //         color: Colors.red,
+      //         textColor: Colors.white,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+    );
+  }
+}
